cb45eaa91c51db6d74058cd02dbcee383d97431a
0122345 <ntwarifiacre043@gmail.com>
2024-09-11 22:41:27 +0200

    here we go 😎

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+   return(
+    <>
+    <p className='text-black font-extrabold text-6xl text-center'>
+       We will be doing sign in and login , auth pro
+    </p>
+    </>
+   )
+}
+
+export default App
