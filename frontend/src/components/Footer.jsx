58f5076f4b0c2c0220951f364ce3d02c7e8d760c
DUSHIME1212 <mdonavan33@gmail.com>
2024-09-14 19:50:08 +0200

    "Updated CategoriesHolder and Footer components"

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,16 +1,9 @@
  
  function Footer () {
   return(
-     <main className="mt-11 ">
-        <div className="flex justify-center items-center bg-footert-white w-full h-[26dvh]">
-             yewe
-        </div>
-        
-        <div className="flex justify-center items-center py-4 bg-footerf-black w-full h-[35dvh] text-white">
-            Footer we came
-        </div>
-           
-     </main>
+     <Footer className="">
+        hello
+     </Footer>
   );
 }  
 
