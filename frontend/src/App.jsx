c9769988f88c2811865db13f86e2c358198073c4
DUSHIME1212 <mdonavan33@gmail.com>
2024-09-14 19:29:02 +0200

    components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ export default function App() {
         <Navbar />
       <Routes>
         <Route path="/" element={<HomepageLanding />}/>
+        
       </Routes>
     </div>
   );
---
+++ frontend/src/utils/utils.js
@@ -0,0 +1,50 @@
+export const categories = [
+    {
+        "image": "/category/image 1.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-1.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-2.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-3.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-4.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-5.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-6.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-7.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-8.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-9.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-10.png",
+        "text":"Fresh Fruit"
+    },
+    {
+        "image": "/category/image 1-11.png",
+        "text":"Fresh Fruit"
+    }
+]
