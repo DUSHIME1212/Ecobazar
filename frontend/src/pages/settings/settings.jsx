11b7371f2607fc9d58b98294f67ec9e76b6911a0
0122345 <ntwarifiacre043@gmail.com>
2024-09-22 20:56:43 +0200

    continuing settings manip

--- frontend/src/pages/settings/settings.jsx
+++ frontend/src/pages/settings/settings.jsx
@@ -2,9 +2,16 @@ import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import Breadcrumbs from "../../components/Breadcrumbs";
 import SideBar from "../../components/SideBar";
+import { countries } from "../../utils/utils";
 
 function AccountSettings() {
   const pathName = useLocation();
+  const [selectedCOuntry, setSelectedCountry] = useState('');
+  const handleCountry = (e) => {
+    setSelectedCountry(e.target.value);
+  };
+  
+
   const [formData, setFormData] = useState({
     firstName: "Dianne",
     lastName: "Russel",
@@ -23,6 +30,7 @@ function AccountSettings() {
   };
 
 
+  //TODO:
   const handleImageChange = (e) => {
     const file = e.target.files[0];
     if (file) {
@@ -34,6 +42,7 @@ function AccountSettings() {
     }
   };
 
+ 
   return (
     <main className="">
       <Breadcrumbs pageroute={pathName.pathname} />
@@ -106,7 +115,7 @@ function AccountSettings() {
                   />
                   <label
                     htmlFor="imageInput"
-                    className="cursor-pointer rounded-full border-2 w-[130px] h-[38px] flex items-center justify-center font-semibold text-green-600 border-solid border-green-600"
+                    className="cursor-pointer rounded-full border-2 lg:w-[130px] w-[90px] h-[40px] text-sm lg:h-[38px] flex items-center justify-center lg:font-semibold  text-green-600 lg:p-0 pl-5 border-solid border-green-600"
                   >
                     Change Image
                   </label>
@@ -114,8 +123,70 @@ function AccountSettings() {
               </div>
             </div>
           </div>
-          <div className="p-4 border rounded-md min-w-[50dvw] h-[38dvh] mt-4">
-            yoo
+
+          {/* TODO: form manipulation */}
+
+          <div className="flex flex-col p-4 border rounded-md w-[93dvw] lg:w-[73dvw] h-full mt-4">
+             <h1>Billing Address</h1>
+             <hr className="w-full h-1" />
+
+             <div className="" id="form">
+               <form action="" method="get">
+               <label htmlFor="firstName">First Name</label>
+                  <input
+                    type="text"
+                    name="firstName"
+                    value={formData.firstName}
+                    onChange={handleInputChange}
+                    className="input_controllers"
+                  />
+                  <label htmlFor="lastName">Last Name</label>
+                  <input
+                    type="text"
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={handleInputChange}
+                    className="input_controllers"
+                  />
+                  <label htmlFor="Company">
+                    Company Name <small>(Option)</small>
+                    in
+                  </label>
+                  <select id="country" value={selectedCOuntry} onChange={handleCountry}>
+                    <option value="">--please choose an option --</option>
+                    {
+                      countries.map((country, index) => (
+                        <option value={country} key={index}>
+                          {country}
+                        </option>
+                      ))
+                    }
+                  </select>
+                      
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleInputChange}
+                    className="input_controllers"
+                  />
+                  <label htmlFor="phoneNumber">Phone Number</label>
+                  <input
+                    type="tel"
+                    name="phoneNumber"
+                    value={formData.phoneNumber}
+                    onChange={handleInputChange}
+                    className="input_controllers"
+                  />
+                  <button
+                    type="submit"
+                    className=" bg-green-600 text-white  rounded-full w-[180px] h-[38px] text-center"
+                  >
+                    Save Changes
+                  </button>
+               </form>
+             </div>
           </div>
         </div>
       </div>
