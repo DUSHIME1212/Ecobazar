c2751359461469499ef1a65d3edc5fa875d801b5
MUGWANEZA MANZI Audace <142017846+MUGWANEZAMANZI@users.noreply.github.com>
2024-10-10 17:33:34 +0200

    testing shop1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import LoginAuth from "./pages/settings/login.jsx";
 import PagenotFound from "./pages/PagenotFound.jsx";
 import FAQs from "./pages/FAQs.jsx";
 import SignUp from "./pages/settings/signIn.jsx";
+import Shop from "./pages/shop.jsx";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/login" element={<LoginAuth />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<PagenotFound />} />
+        <Route path="/shop" element={<Shop/>} />
       </Routes>
       <Footer />
     </div>
---
+++ frontend/src/pages/shop.jsx
@@ -0,0 +1,15 @@
+import { useLocation } from "react-router-dom";
+
+
+
+
+
+function Shop(){
+    
+    return (
+        <div id="newsLetterPopUp">
+            <img src="C:\Users\USER 1\Ecobazar\frontend\public\imageNews.png" width="354px" height="380px" />
+        </div>
+    );
+}
+export default Shop;
